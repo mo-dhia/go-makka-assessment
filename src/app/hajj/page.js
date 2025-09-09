943bd5795a52f7d3e2e41120e337f69ddc1855aa
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-09-09 11:00:40 +0100

    global: set body to correct bg color "background-base"

--- src/app/hajj/page.js
+++ src/app/hajj/page.js
@@ -1,13 +1,23 @@
 import Banner from "../../../components/banner/banner";
+import HajjContextSection from "../../../sections/Hajj/context/context";
 
 export default function HajjPage() {
   return (
-    <main>
+    <main style={{ display: 'flex', flexDirection: 'column', gap: '48px' }}>
       <Banner
         title="Hajj 2026 avec"
         highlight="GO-MAKKAH"
         subtitle="Comprendre, choisir et réussir votre pèlerinage .."
       />
+
+      <div style={{ display: 'flex', gap: '24px' }}>
+        <div>
+          <HajjContextSection />
+        </div>
+        <div style={{ height: '100%', width: '20%' }}>
+
+        </div>
+      </div>
     </main>
   );
 }
