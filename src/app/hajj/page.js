d401b604783b011f781d0b14432ef4ec548c499d
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-09-09 13:28:09 +0100

    sections(acc): flex system responsive

--- src/app/hajj/page.js
+++ src/app/hajj/page.js
@@ -1,21 +1,31 @@
 import Banner from "../../../components/banner/banner";
 import HajjContextSection from "../../../sections/Hajj/context/context";
+import SupportTechniqueSection from "../../../sections/Hajj/supportTechnique/supportTechnique";
+import Issues from "../../../sections/issues/issues";
+import Solutions from "../../../sections/solutions/solutions";
+import styles from "./hajj.module.css";
 
 export default function HajjPage() {
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', gap: '48px' }}>
+    <main className={styles.main}>
       <Banner
         title="Hajj 2026 avec"
         highlight="GO-MAKKAH"
         subtitle="Comprendre, choisir et réussir votre pèlerinage .."
       />
 
-      <div style={{ display: 'flex', gap: '24px' }}>
-        <div>
+      <div className={styles.row}>
           <HajjContextSection />
+        <div className={styles.fullHeight}>
+          <SupportTechniqueSection />
         </div>
-        <div style={{ height: '100%', width: '20%' }}>
+      </div>
 
+
+      <div className={styles.row}>
+          <Issues />
+        <div className={`${styles.fullHeight} ${styles.solutionsRight}`}>
+          <Solutions />
         </div>
       </div>
     </main>
