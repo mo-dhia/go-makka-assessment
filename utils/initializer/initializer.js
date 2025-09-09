13a9876f5cf6ae10170e0f97f81705281f6df14b
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-09-09 09:33:28 +0100

    initiliazer: redirect to hajj on first page load

--- utils/initializer/initializer.js
+++ utils/initializer/initializer.js
@@ -2,8 +2,11 @@
 
 import { useEffect } from "react";
 import { dimensionsStore } from "@/../store/dimensionStore";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Initializer() {
+  const router = useRouter();
+  const pathname = usePathname();
   useEffect(() => {
     if (typeof window === "undefined") return;
 
@@ -14,6 +17,14 @@ export default function Initializer() {
 
     updateViewportWidth();
     window.addEventListener("resize", updateViewportWidth);
+
+    // Redirect to Hajj on first load from home page
+    try {
+      const currentPath = pathname || window.location.pathname;
+      if (currentPath === "/") {
+        router.replace("/hajj");
+      }
+    } catch {}
     return () => {
       window.removeEventListener("resize", updateViewportWidth);
     };
