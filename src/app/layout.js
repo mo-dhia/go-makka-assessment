79bd3d3b95ad5dfcbfa69c0b279f9ddf648e9113
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-09-09 06:23:02 +0100

    global(add): color, font, space variables

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,22 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/../components/layout/header/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
+  weight: [
+    "400",
+    "500",
+    "600",
+    "700",
+    "800"
+  ],
 });
 
 export const metadata = {
@@ -20,7 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable} ${montserrat.variable}`}>
         <Header />
         {children}
       </body>
